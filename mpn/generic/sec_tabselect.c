264fbc9dc1cb947942e1bc00cde8cdde31b57c1f
Torbjorn Granlund <tg@gmplib.org>
2021-11-07 22:40:43 +0100

    Make recently added code actually work.

--- mpn/generic/sec_tabselect.c
+++ mpn/generic/sec_tabselect.c
@@ -28,6 +28,7 @@ You should have received copies of the GNU General Public License and the
 GNU Lesser General Public License along with the GNU MP Library.  If not,
 see https://www.gnu.org/licenses/.  */
 
+#include <stdio.h>
 #include "gmp-impl.h"
 
 #ifndef SEC_TABSELECT_METHOD
@@ -78,6 +79,38 @@ mpn_sec_tabselect (volatile mp_limb_t * restrict rp,
   mp_limb_t mask, r0, r1, r2, r3;
   volatile const mp_limb_t * restrict tp;
 
+  if (n & 1)
+    {
+      tp = tab;
+      r0 = 0;
+      for (k = 0; k < nents; k++)
+	{
+	  mask = (mp_limb_t) ((-(unsigned long) (which ^ k)) >> (BITS_PER_ULONG - 1)) - 1;
+	  r0 += tp[0] & mask;
+	  tp += n;
+	}
+      rp[0] = r0;
+      rp += 1;
+      tab += 1;
+    }
+
+  if (n & 2)
+    {
+      tp = tab;
+      r0 = r1 = 0;
+      for (k = 0; k < nents; k++)
+	{
+	  mask = (mp_limb_t) ((-(unsigned long) (which ^ k)) >> (BITS_PER_ULONG - 1)) - 1;
+	  r0 += tp[0] & mask;
+	  r1 += tp[1] & mask;
+	  tp += n;
+	}
+      rp[0] = r0;
+      rp[1] = r1;
+      rp += 2;
+      tab += 2;
+    }
+
   for (i = 0; i <= n - 4; i += 4)
     {
       tp = tab + i;
@@ -96,35 +129,6 @@ mpn_sec_tabselect (volatile mp_limb_t * restrict rp,
       rp[2] = r2;
       rp[3] = r3;
       rp += 4;
-    }
-
-  if (n & 2 != 0)
-    {
-      tp = tab + n - 2;
-      r0 = r1 = 0;
-      for (k = 0; k < nents; k++)
-	{
-	  mask = (mp_limb_t) ((-(unsigned long) (which ^ k)) >> (BITS_PER_ULONG - 1)) - 1;
-	  r0 += tp[0] & mask;
-	  r1 += tp[1] & mask;
-	  tp += n;
-	}
-      rp[0] = r0;
-      rp[1] = r1;
-      rp += 2;
-    }
-
-  if (n & 1 != 0)
-    {
-      tp = tab + n - 1;
-      r0 = r1 = 0;
-      for (k = 0; k < nents; k++)
-	{
-	  mask = (mp_limb_t) ((-(unsigned long) (which ^ k)) >> (BITS_PER_ULONG - 1)) - 1;
-	  r0 += tp[0] & mask;
-	  tp += n;
-	}
-      rp[0] = r0;
     }
 }
 #endif
