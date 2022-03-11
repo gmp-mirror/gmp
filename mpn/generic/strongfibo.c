63ba22170e0e257adba3680f13b721598bd1e17e
Marco Bodrato <bodrato@mail.dm.unipi.it>
2022-03-11 09:34:15 +0100

    Copyright years

--- mpn/generic/strongfibo.c
+++ mpn/generic/strongfibo.c
@@ -6,7 +6,7 @@ Contributed to the GNU project by Marco Bodrato.
    CERTAIN TO BE SUBJECT TO INCOMPATIBLE CHANGES OR DISAPPEAR COMPLETELY IN
    FUTURE GNU MP RELEASES.
 
-Copyright 2001, 2002, 2005, 2009, 2018 Free Software Foundation, Inc.
+Copyright 2001, 2002, 2005, 2009, 2018, 2022 Free Software Foundation, Inc.
 
 This file is part of the GNU MP Library.
 
