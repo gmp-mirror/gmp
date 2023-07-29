0ab4aa2d7b6b7c3a1da3daeff959dc880d72c616
Torbjorn Granlund <tg@gmplib.org>
2023-07-29 15:07:46 +0200

    Rewrite z13 mul_basecase, using new addmul_1.

--- mpn/s390_64/z13/mul_basecase.asm
+++ mpn/s390_64/z13/mul_basecase.asm
@@ -33,191 +33,207 @@ include(`../config.m4')
 
 C INPUT PARAMETERS
 define(`rp',	`%r2')
-define(`up',	`%r3')
-define(`un',	`%r4')
-define(`vp',	`%r5')
-define(`vn_arg',`%r6')
-
-define(`vn',    `%r13')
+define(`ap',	`%r3')
+define(`an',	`%r4')	C 32
+define(`bp',	`%r5')	C 40
+define(`bn',	`%r6')	C 48
 
 define(`idx',	`%r14')
-define(`v0',	`%r11')
-define(`v1',	`%r12')
+define(`b0',	`%r10')
+
+dnl live in addmul_1:
+dnl r0  r1  r2  r3  r4  r5  r6  r7  r8  r9 r10 r11 r12 r13 r14
+dnl xx  xx  rp  ap  an  bp  xx  xx  xx  xx  b0  i   xx  xx idx
+dnl stack: bn
+
+dnl TODO
+dnl  * Have mul_1 start without initial (un mod 4) separation, instead handle
+dnl    after loop.  Then fall into 4 separate addmul_1 loops.
+dnl  * Streamline handling of bn, an, %r11 to reduce the # if memops.
 
 define(`MUL_1',`
 pushdef(`L',
 defn(`L')$1`'_m1)
+	vzero	%v2
+	srlg	%r11, %r0, 2
 
-	vzero	%v29
-	lghi	%r9, 0
-	tmll	un, 1
-	srlg	un, un, 1
-	je	L(evn)
-L(odd):	lg	%r7, 0(up)
-	mlgr	%r6, v0			C W1 W0
-	stg	%r7, 0(rp)
-	lghi	idx, 8
-	clgije	un, 0, L(end)		C FIXME: Done, return!
-	j	L(top)
-L(evn):	lghi	%r6, 0
-	lghi	idx, 0
+	tmll	%r0, 1
+	je	L(bx0)
+L(bx1):	tmll	%r0, 2
+	jne	L(b11)
 
-L(top):	lgr	%r9, %r6
-	lg	%r1, 0(idx, up)
-	lg	%r7, 8(idx, up)
-	mlgr	%r0, v0			C W1 W0
-	mlgr	%r6, v0			C W2 W1
-	vlvgp	%v23, %r0, %r1		C W1 W0
-	vlvgp	%v21, %r7, %r9		C W1 W0
-	vacq	%v20, %v23, %v21, %v29	C
-	vacccq	%v29, %v23, %v21, %v29	C	carry critical path 3
-	vpdi	%v20, %v20, %v20, 4
-	vst	%v20, 0(idx, rp), 3
-	la	idx, 16(idx)
-	brctg	un, L(top)
+L(b01):	lghi	idx, -24
+	lg	%r13, 0(ap)
+	mlgr	%r12, b0
+	stg	%r13, 0(rp)
+	cgijne	%r11, 0, L(cj0)
 
-L(end):	vlgvg	%r7, %v29, 1
-	algr	%r6, %r7
-	stg	%r6, 0(idx, rp)
+L(1):	stg	%r12, 8(rp)
+	lmg	%r6, %r14, 48(%r15)
+	br	%r14
+
+L(b11):	lghi	idx, -8
+	lg	%r9, 0(ap)
+	mlgr	%r8, b0
+	stg	%r9, 0(rp)
+	j	L(cj1)
+
+L(bx0):	tmll	%r0, 2
+	jne	L(b10)
+L(b00):	lghi	idx, -32
+	lghi	%r12, 0
+L(cj0):	lg	%r1, 32(idx, ap)
+	lg	%r9, 40(idx, ap)
+	mlgr	%r0, b0
+	mlgr	%r8, b0
+	vlvgp	%v6, %r0, %r1
+	vlvgp	%v7, %r9, %r12
+	j	L(mid)
+
+L(b10):	lghi	idx, -16
+	lghi	%r8, 0
+L(cj1):	lg	%r7, 16(idx, ap)
+	lg	%r13, 24(idx, ap)
+	mlgr	%r6, b0
+	mlgr	%r12, b0
+	vlvgp	%v6, %r6, %r7
+	vlvgp	%v7, %r13, %r8
+	cgije	%r11, 0, L(end)
+
+L(top):	lg	%r1, 32(idx, ap)
+	lg	%r9, 40(idx, ap)
+	mlgr	%r0, b0
+	mlgr	%r8, b0
+	vacq	%v3, %v6, %v7, %v2
+	vacccq	%v2, %v6, %v7, %v2
+	vpdi	%v3, %v3, %v3, 4
+	vst	%v3, 16(idx, rp), 3
+	vlvgp	%v6, %r0, %r1
+	vlvgp	%v7, %r9, %r12
+L(mid):	lg	%r7, 48(idx, ap)
+	lg	%r13, 56(idx, ap)
+	mlgr	%r6, b0
+	mlgr	%r12, b0
+	vacq	%v1, %v6, %v7, %v2
+	vacccq	%v2, %v6, %v7, %v2
+	vpdi	%v1, %v1, %v1, 4
+	vst	%v1, 32(idx, rp), 3
+	vlvgp	%v6, %r6, %r7
+	vlvgp	%v7, %r13, %r8
+	la	idx, 32(idx)
+	brctg	%r11, L(top)
+
+L(end):	vacq	%v3, %v6, %v7, %v2
+	vacccq	%v2, %v6, %v7, %v2
+	vpdi	%v3, %v3, %v3, 4
+	vst	%v3, 16(idx, rp), 3
+
+	vlgvg	%r0, %v2, 1
+	algr	%r0, %r12
+	stg	%r0, 32(idx, rp)
 popdef(`L')
 ')
 
-define(`MUL_2',`
+define(`ADDMUL_1',`
 pushdef(`L',
-defn(`L')$1`'_m2)
-	vzero	%v27
-	vzero	%v28
-	vzero	%v29
-	vzero	%v30
-	lghi	%r10, 0
-	lg	v0, 0(vp)
-	lg	v1, 8(vp)
-	tmll	un, 1
-	srlg	un, un, 1
-	je	L(evn)
+defn(`L')$1`'_am1)
+	vzero	%v0
+	vzero	%v2
+	srlg	%r11, %r0, 2
 
-L(odd):	lg	%r7, 0(up)
-	mlgr	%r6, v0			C W2 W1
-	lg	%r1, 0(up)
-	stg	%r7, 0(rp)
-	lghi	idx, 8
-dnl	clgije	un, 0, L(end)
-	j	L(top)
+	tmll	%r0, 1
+	je	L(bx0)
+L(bx1):	tmll	%r0, 2
+	jne	L(b11)
 
-L(evn):	lghi	%r6, 0
-	lghi	idx, 0
-	lghi	%r1, 0
+L(b01):	lghi	idx, -24
+	vleg	%v2, 0(rp), 1
+	lg	%r13, 0(ap)
+	vzero	%v4
+	mlgr	%r12, b0
+	vlvgg	%v4, %r13, 1
+	vaq	%v2, %v2, %v4
+	vsteg	%v2, 0(rp), 1
+	vmrhg	%v2, %v2, %v2
+	j	L(cj0)
 
-L(top):	lg	%r9, 0(idx, up)
-	mlgr	%r0, v1			C W2 W1
-	mlgr	%r8, v1			C W3 W2
-	vlvgp	%v22, %r0, %r1		C W2 W1
-	vlvgp	%v23, %r9, %r6		C W2 W1
-	lg	%r1, 0(idx, up)
-	lg	%r7, 8(idx, up)
-	mlgr	%r0, v0			C W2 W1
-	mlgr	%r6, v0			C W3 W2
-	vlvgp	%v20, %r0, %r1		C W2 W1
-	vlvgp	%v21, %r7, %r10		C W2 W1
-	vacq	%v24, %v22, %v23, %v27	C
-	vacccq	%v27, %v22, %v23, %v27	C	carry critical path 1
-	vacq	%v23, %v24, %v20, %v28	C
-	vacccq	%v28, %v24, %v20, %v28	C	carry critical path 2
-	vacq	%v20, %v23, %v21, %v29	C
-	vacccq	%v29, %v23, %v21, %v29	C	carry critical path 3
-	vpdi	%v20, %v20, %v20, 4
-	lg	%r1, 8(idx, up)
-	vst	%v20, 0(idx, rp), 3
-	lgr	%r10, %r8
-	la	idx, 16(idx)
-	brctg	un, L(top)
+L(b11):	lghi	idx, -8
+	vleg	%v2, 0(rp), 1
+	lg	%r9, 0(ap)
+	vzero	%v4
+	mlgr	%r8, b0
+	vlvgg	%v4, %r9, 1
+	vaq	%v2, %v2, %v4
+	vsteg	%v2, 0(rp), 1
+	vmrhg	%v2, %v2, %v2
+	j	L(cj1)
 
-L(end):	mlgr	%r0, v1
-	algr	%r1, %r6
-	alcgr	%r0, un
-	algr	%r1, %r8
-	alcgr	%r0, un
-	vag	%v27, %v27, %v28
-	vag	%v29, %v29, %v30
-	vag	%v27, %v27, %v29
-	vlgvg	%r10, %v27, 1
-	algr	%r1, %r10
-	stg	%r1, 0(idx, rp)
-	alcgr	%r0, un
-	stg	%r0, 8(idx, rp)
-popdef(`L')
-')
+L(bx0):	tmll	%r0, 2
+	jne	L(b10)
+L(b00):	lghi	idx, -32
+	lghi	%r12, 0
+L(cj0):	lg	%r1, 32(idx, ap)
+	lg	%r9, 40(idx, ap)
+	mlgr	%r0, b0
+	mlgr	%r8, b0
+	vlvgp	%v6, %r0, %r1
+	vlvgp	%v7, %r9, %r12
+	j	L(mid)
 
-define(`ADDMUL_2',`
-pushdef(`L',
-defn(`L')$1`'_am2)
-	vzero	%v27
-	vzero	%v28
-	vzero	%v29
-	vzero	%v30
-	lghi	%r10, 0
-	lg	v0, 0(vp)
-	lg	v1, 8(vp)
-	tmll	un, 1
-	srlg	un, un, 1
-	je	L(evn)
+L(b10):	lghi	idx, -16
+	lghi	%r8, 0
+L(cj1):	lg	%r7, 16(idx, ap)
+	lg	%r13, 24(idx, ap)
+	mlgr	%r6, b0
+	mlgr	%r12, b0
+	vlvgp	%v6, %r6, %r7
+	vlvgp	%v7, %r13, %r8
+	cgije	%r11, 0, L(end)
 
-L(odd):	lg	%r7, 0(up)
-	mlgr	%r6, v0			C W2 W1
-	alg	%r7, 0(rp)
-	alcgr	%r6, %r10
-	stg	%r7, 0(rp)
-	lghi	idx, 8
-dnl	clgije	un, 0, L(end)
-	j	L(top)
+L(top):	lg	%r1, 32(idx, ap)
+	lg	%r9, 40(idx, ap)
+	mlgr	%r0, b0
+	mlgr	%r8, b0
+	vl	%v1, 16(idx, rp), 3
+	vpdi	%v1, %v1, %v1, 4
+	vacq	%v5, %v6, %v1, %v0
+	vacccq	%v0, %v6, %v1, %v0
+	vacq	%v3, %v5, %v7, %v2
+	vacccq	%v2, %v5, %v7, %v2
+	vpdi	%v3, %v3, %v3, 4
+	vst	%v3, 16(idx, rp), 3
+	vlvgp	%v6, %r0, %r1
+	vlvgp	%v7, %r9, %r12
+L(mid):	lg	%r7, 48(idx, ap)
+	lg	%r13, 56(idx, ap)
+	mlgr	%r6, b0
+	mlgr	%r12, b0
+	vl	%v4, 32(idx, rp), 3
+	vpdi	%v4, %v4, %v4, 4
+	vacq	%v5, %v6, %v4, %v0
+	vacccq	%v0, %v6, %v4, %v0
+	vacq	%v1, %v5, %v7, %v2
+	vacccq	%v2, %v5, %v7, %v2
+	vpdi	%v1, %v1, %v1, 4
+	vst	%v1, 32(idx, rp), 3
+	vlvgp	%v6, %r6, %r7
+	vlvgp	%v7, %r13, %r8
+	la	idx, 32(idx)
+	brctg	%r11, L(top)
 
-L(evn):	lghi	%r6, 0
-	lghi	idx, 0
-	lghi	%r1, 0
-	j	L(ent)
+L(end):	vl	%v1, 16(idx, rp), 3
+	vpdi	%v1, %v1, %v1, 4
+	vacq	%v5, %v6, %v1, %v0
+	vacccq	%v0, %v6, %v1, %v0
+	vacq	%v3, %v5, %v7, %v2
+	vacccq	%v2, %v5, %v7, %v2
+	vpdi	%v3, %v3, %v3, 4
+	vst	%v3, 16(idx, rp), 3
 
-L(top):	lg	%r1, -8(idx, up)
-L(ent):	lg	%r9, 0(idx, up)
-	mlgr	%r0, v1			C W2 W1
-	mlgr	%r8, v1			C W3 W2
-	vlvgp	%v22, %r0, %r1		C W2 W1
-	vlvgp	%v23, %r9, %r6		C W2 W1
-	lg	%r1, 0(idx, up)
-	lg	%r7, 8(idx, up)
-	mlgr	%r0, v0			C W2 W1
-	mlgr	%r6, v0			C W3 W2
-	vlvgp	%v20, %r0, %r1		C W2 W1
-	vlvgp	%v21, %r7, %r10		C W2 W1
-	vacq	%v24, %v22, %v23, %v27	C
-	vacccq	%v27, %v22, %v23, %v27	C	carry critical path 1
-	vacq	%v23, %v24, %v20, %v28	C
-	vacccq	%v28, %v24, %v20, %v28	C	carry critical path 2
-	vacq	%v24, %v23, %v21, %v29	C
-	vacccq	%v29, %v23, %v21, %v29	C	carry critical path 3
-	vl	%v16, 0(idx, rp), 3
-	vpdi	%v16, %v16, %v16, 4
-	vacq	%v20, %v24, %v16, %v30	C
-	vacccq	%v30, %v24, %v16, %v30	C	carry critical path 4
-	vpdi	%v20, %v20, %v20, 4
-	vst	%v20, 0(idx, rp), 3
-	lgr	%r10, %r8
-	la	idx, 16(idx)
-	brctg	un, L(top)
-
-L(end):	lg	%r1, -8(idx, up)
-	mlgr	%r0, v1
-	algr	%r1, %r6
-	alcgr	%r0, un
-	algr	%r1, %r8
-	alcgr	%r0, un
-	vag	%v27, %v27, %v28
-	vag	%v29, %v29, %v30
-	vag	%v27, %v27, %v29
-	vlgvg	%r10, %v27, 1
-	algr	%r1, %r10
-	stg	%r1, 0(idx, rp)
-	alcgr	%r0, un
-	stg	%r0, 8(idx, rp)
+	vag	%v2, %v0, %v2
+	vlgvg	%r0, %v2, 1
+	algr	%r0, %r12
+	stg	%r0, 32(idx, rp)
 popdef(`L')
 ')
 
@@ -226,30 +242,23 @@ ASM_START()
 
 PROLOGUE(mpn_mul_basecase)
 	stmg	%r4, %r14, 32(%r15)
-	lgr	vn, vn_arg
 
-	tmll	vn, 1
-	je	L(vn_evn)
-L(vn_odd):
-	lg	v0, 0(vp)
-	MUL_1()
-	lay	vp, -8(vp)
-	lay	rp, -8(rp)
-	j	L(join)
-L(vn_evn):
-	MUL_2()
-	lay	vn, -2(vn)
-L(join):
-	srlg	vn, vn, 1
-	cgije	vn, 0, L(oend)
-L(otop):
-	lg	un, 32(%r15)
-	la	rp, 16(rp)
-	la	vp, 16(vp)
-	ADDMUL_2()
-	brctg	vn, L(otop)
-L(oend):
+	lgr	%r4, bn
 
-	lmg	%r6, %r14, 48(%r15)
+	lg	%r0, 32(%r15)
+	lg	b0, 0(bp)
+	MUL_1()			C implicitly pass r0 = an
+
+	aghi	%r4, -1
+	je	L(end)
+L(top):	lg	%r0, 32(%r15)
+	la	bp, 8(bp)
+	la	rp, 8(rp)
+	lg	b0, 0(bp)
+	ADDMUL_1()		C implicitly pass r0 = an
+	brctg	%r4, L(top)
+
+L(end):	lmg	%r6, %r14, 48(%r15)
 	br	%r14
 EPILOGUE()
+	.section	.note.GNU-stack
